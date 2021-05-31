0ad81c66871547c05bf00efb0a95ad3dddfaf83e
xavier290 <xa25alex@gmail.com>
2021-05-31 16:28:18 -0600

    implementing react

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
